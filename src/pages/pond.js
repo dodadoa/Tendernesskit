73ccec457c6ff07253a375319db9942b82597a9b
dodadoa <dodadoa@Avas-Genesis.local>
2021-04-14 01:35:41 +0700

    add link back to fish garden

--- src/pages/pond.js
+++ src/pages/pond.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import styled from "@emotion/styled"
 import PageTransition from 'gatsby-plugin-page-transitions'
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import background from "../images/pond_bg.jpg"
 import pond from "../images/pond_animation.gif"
@@ -21,16 +22,33 @@ const FrontPageBackground = styled.div`
 
 const pondStyle = {
   width: '1200px',
-  position: 'absolute',
+  position: 'relative',
   bottom: '50px',
   left: '150px'
 }
+
+const FishWrapper = styled.div`
+  width: 40px;
+  height: 40px;
+  margin: 0;
+  left: 330px;
+  top: 550px;
+  position: absolute;
+  z-index: 999;
+  /* background-color: black; */
+  &:hover {
+    cursor: pointer;
+  }
+`
 
 const Pond = () => {
   return (
     <Layout title="pond">
       <PageTransition>
         <FrontPageBackground>
+          <Link to="/fishGarden">
+            <FishWrapper />
+          </Link>
           <img style={pondStyle} src={pond} alt="pond" />
         </FrontPageBackground>
       </PageTransition>
