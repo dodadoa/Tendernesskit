953309dfe99d132cd51d7315899f00b419bcd2e2
dodadoa <dodadoa@Avas-Genesis.local>
2021-04-25 18:36:06 +0700

    forces viewer to use chrome

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import './layout.css'
+import styled from "@emotion/styled"
 import { useStaticQuery, graphql } from 'gatsby'
+import { detect } from 'detect-browser'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
 import thumbnail from '../images/thumbnail.png'
@@ -22,6 +24,35 @@ const Layout = ({ description, lang, meta, title, children  }) => {
 
   const metaDescription = description || site.siteMetadata.description
   const defaultTitle = site.siteMetadata?.title
+
+  const browser = detect()
+  console.log(browser)
+  const isChrome = browser.name === 'chrome'
+  if (!isChrome) {
+    const FrontPageBackground = styled.div`
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      margin: 0;
+      background-image: linear-gradient(to bottom, #9e9e, #eee);
+      width: 100%;
+      height: 100%;
+    `
+    return (
+      <FrontPageBackground>
+        <p style={{
+          textAlign: 'center',
+          width: '100%',
+          fontFamily: 'Spectral',
+          fontSize: '20px'
+        }}>
+          please use chrome in desktop for fully experienced
+        </p>
+      </FrontPageBackground>
+    )
+  }
 
   return (
     <>
