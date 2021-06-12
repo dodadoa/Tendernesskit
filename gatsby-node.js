a25ee26fd2e1618dc427b7dce9ee290a92bb7cfd
dodadoa <dodadoa@Avas-Genesis.local>
2021-06-13 05:17:23 +0700

    firebase passing

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,14 @@
-exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
-  actions.setWebpackConfig({
-    externals: getConfig().externals.concat(function(context, request, callback) {
-      const regex = /^@?firebase(\/(.+))?/;
-      if (regex.test(request)) {
-        return callback(null, `umd ${request}`);
-      }
-      callback();
-    }),
-  });
-};
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /firebase/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
