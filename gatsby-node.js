1b1dea5867362bbe36e4fe1f57caa77521f5382c
dodadoa <dodadoa@Avas-Genesis.local>
2021-06-13 05:04:02 +0700

    try to fix webpack things

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,11 @@
 exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
-  if (stage === 'build-html') {
-    actions.setWebpackConfig({
-      externals: getConfig().externals.concat(function(context, request, callback) {
-        const regex = /^@?firebase(\/(.+))?/;
-        if (regex.test(request)) {
-          return callback(null, `umd ${request}`);
-        }
-        callback();
-      }),
-    });
-  }
+  actions.setWebpackConfig({
+    externals: getConfig().externals.concat(function(context, request, callback) {
+      const regex = /^@?firebase(\/(.+))?/;
+      if (regex.test(request)) {
+        return callback(null, `umd ${request}`);
+      }
+      callback();
+    }),
+  });
 };
